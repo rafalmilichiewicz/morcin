a2eca241b1345c5e9511ca994cada3956a93c9ac
Angelones12 <mikoajc8@gmail.com>
2024-05-29 13:54:45 +0200

    dont know how to fix it

--- Dockerized/DB/init.sql
+++ Dockerized/DB/init.sql
@@ -1,6 +1,10 @@
+-- Informacja o wywołaniu skryptu
+SELECT 'Inicjalizacja bazy danych rozpoczęta...' AS 'Komunikat';
+
+-- Tworzenie bazy danych, tabeli, itp.
 CREATE DATABASE IF NOT EXISTS dogs_db;
 
-USE dogs_db
+USE dogs_db;
 
 CREATE TABLE IF NOT EXISTS dogs(
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,3 +12,6 @@ CREATE TABLE IF NOT EXISTS dogs(
     curiosity VARCHAR(100) NOT NULL,
     obrazek BLOB(65535) NOT NULL
 );
+
+-- Informacja o zakończeniu wywołania skryptu
+SELECT 'Inicjalizacja bazy danych zakończona.' AS 'Komunikat';
